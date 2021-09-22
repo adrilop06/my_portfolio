c9353aa01d96e18e9e07c0ceb6d120b75c33f516
Adrian Lopez <adrianlopezpazos@gmail.com>
2021-09-22 10:59:38 -0500

    v4

--- src/components/Body/SkillsPage.vue
+++ src/components/Body/SkillsPage.vue
@@ -47,7 +47,7 @@ export default {
      
        data() {
         return {
-          height: 300,
+          height:300,
           isShow: {
             'ChartWeb': false,
             'ChartGame': false,
